28ec95300aa526a9cd1e7ca72e6a21f3028a04ce
mukuld86 <mukuldeshwal86@gmail.com>
2026-08-03 22:52:50 +0530

    Add project files.

---
+++ StudentManagement.API/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagement.API.Models
+{
+    public class Student
+    {
+        [Key]
+        public int RegistrationNumber { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [StringLength(30)]
+        public string Course { get; set; } = string.Empty;
+        [Range(18, 60)]
+        public int Age { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+    }
+}
